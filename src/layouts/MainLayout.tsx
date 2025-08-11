396ef17e942cdd0608d9ac8deb01b7f22f3025cf
coderZhen258 <coderZhen@foxmail.com>
2025-08-11 17:11:46 +0800

    feat: antd5引入，配置首页模板

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,29 @@
+/*
+ * @Author: 唐宇
+ * @Date: 2025-08-05 16:32:16
+ * @LastEditors: 唐宇
+ * @LastEditTime: 2025-08-11 17:08:33
+ * @FilePath: \survey-frontend\src\layouts\MainLayout.tsx
+ * @Description: 首页模板
+ *
+ * Copyright (c) 2025 by 唐宇, All Rights Reserved.
+ */
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
+import { Layout } from 'antd';
+import styles from './MainLayout.module.scss';
+
+const { Header, Footer, Content } = Layout;
 
 const MainLayout: FC = () => {
   return (
-    <>
-      <div>MainLayout header</div>
-      <div>
+    <Layout className={styles.layout}>
+      <Header className={styles.header}>MainLayout header</Header>
+      <Content className={styles.content}>
         <Outlet />
-      </div>
-      <div>MainLayout footer</div>
-    </>
+      </Content>
+      <Footer className={styles.footer}>小慕问卷 &copy;2025 - present. Created by tangyu</Footer>
+    </Layout>
   );
 };
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React, { FC } from 'react';
+import { Button } from 'antd';
 
 const Home: FC = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <p>Home</p>
+      <Button> antd button </Button>
+    </div>
+  );
 };
 
 export default Home;
