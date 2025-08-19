4a8b0af19c8832472fd4ab742f7c5a9f7c42d0c1
coderZhen258 <coderZhen@foxmail.com>
2025-08-19 17:20:35 +0800

    feat: 注册页开发及完善表单验证逻辑

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,108 @@
 import React, { FC } from 'react';
+import { Typography, Space, Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
+import { REGISTER_PATHNAME } from '../router';
+import styles from './Login.module.scss';
+
+const { Title } = Typography;
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  const [form] = Form.useForm();
+
+  // 初始化时检查本地存储
+  React.useEffect(() => {
+    const savedUser = localStorage.getItem('savedUser');
+    if (savedUser) {
+      const { username, password, remember } = JSON.parse(savedUser);
+      form.setFieldsValue({ username, password, remember });
+    }
+  }, [form]);
+
+  const finishHandler = (values: any) => {
+    console.log('表单数据:', values);
+
+    // 处理"记住我"功能
+    if (values.remember) {
+      localStorage.setItem(
+        'savedUser',
+        JSON.stringify({
+          username: values.username,
+          password: values.password,
+          remember: true,
+        })
+      );
+    } else {
+      localStorage.removeItem('savedUser');
+    }
+
+    // TODO: 待登录API接口提供后实现
+    // 登录API调用示例：
+    /*
+    try {
+      const res = await axios.post('/api/login', values);
+      if (res.data.code === 0) {
+        message.success('登录成功');
+        // 登录成功后跳转到首页
+        nav('/');
+      } else {
+        message.error(res.data.msg || '登录失败');
+      }
+    } catch (e) {
+      message.error('登录请求失败');
+      console.error('登录请求异常:', e);
+    }
+    */
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <Space>
+          <Title level={2}>
+            <UserOutlined></UserOutlined>
+          </Title>
+          <Title level={2}>用户登录</Title>
+        </Space>
+      </div>
+      <div>
+        <Form form={form} labelCol={{ span: 6 }} wrapperCol={{ span: 20 }} onFinish={finishHandler}>
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { min: 4, message: '用户名至少4个字符' },
+              { max: 20, message: '用户名最多20个字符' },
+            ]}
+          >
+            <Input placeholder="请输入用户名"></Input>
+          </Form.Item>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              { required: true, message: '请输入密码' },
+              { min: 6, message: '密码至少6位' },
+            ]}
+          >
+            <Input.Password placeholder="请输入密码"></Input.Password>
+          </Form.Item>
+          <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 20 }}>
+            <Checkbox>记住我</Checkbox>
+          </Form.Item>
+          <Form.Item wrapperCol={{ offset: 6, span: 20 }}>
+            <Space>
+              <Button type="primary" htmlType="submit">
+                登录
+              </Button>
+              <Link to={REGISTER_PATHNAME}>没有账号，去注册</Link>
+            </Space>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,7 +2,7 @@
  * @Author: 唐宇
  * @Date: 2025-08-04 16:21:18
  * @LastEditors: 唐宇
- * @LastEditTime: 2025-08-15 17:18:19
+ * @LastEditTime: 2025-08-19 16:39:31
  * @FilePath: \survey-frontend\src\pages\Register.tsx
  * @Description: 注册页
  *
@@ -19,7 +19,24 @@ const { Title } = Typography;
 
 const Register: FC = () => {
   const finishHandler = (values: any) => {
-    console.log(values);
+    console.log('表单数据:', values);
+    // TODO: 待注册API接口提供后实现
+    // 注册API调用示例：
+    /*
+    try {
+      const res = await axios.post('/api/register', values);
+      if (res.data.code === 0) {
+        message.success('注册成功');
+        // 注册成功后跳转到登录页
+        nav(LOGIN_PATHNAME);
+      } else {
+        message.error(res.data.msg || '注册失败');
+      }
+    } catch (e) {
+      message.error('注册请求失败');
+      console.error('注册请求异常:', e);
+    }
+    */
   };
   return (
     <div className={styles.container}>
@@ -33,17 +50,52 @@ const Register: FC = () => {
       </div>
       <div>
         <Form labelCol={{ span: 6 }} wrapperCol={{ span: 20 }} onFinish={finishHandler}>
-          <Form.Item label="用户名" name="username">
-            <Input></Input>
+          <Form.Item
+            label="用户名"
+            name="username"
+            rules={[
+              { required: true, message: '请输入用户名' },
+              { min: 4, message: '用户名至少4个字符' },
+              { max: 20, message: '用户名最多20个字符' },
+            ]}
+          >
+            <Input placeholder="4-20个字符"></Input>
           </Form.Item>
-          <Form.Item label="密码" name="password">
-            <Input.Password></Input.Password>
+          <Form.Item
+            label="密码"
+            name="password"
+            rules={[
+              { required: true, message: '请输入密码' },
+              { min: 6, message: '密码至少6位' },
+              { pattern: /^(?=.*[A-Za-z])(?=.*\d).+$/, message: '密码需包含字母和数字' },
+            ]}
+          >
+            <Input.Password placeholder="至少6位，包含字母和数字"></Input.Password>
           </Form.Item>
-          <Form.Item label="确认密码" name="confirmPassword">
-            <Input.Password></Input.Password>
+          <Form.Item
+            label="确认密码"
+            name="confirmPassword"
+            dependencies={['password']}
+            rules={[
+              { required: true, message: '请确认密码' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('password') === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(new Error('两次输入的密码不一致'));
+                },
+              }),
+            ]}
+          >
+            <Input.Password placeholder="再次输入密码"></Input.Password>
           </Form.Item>
-          <Form.Item label="昵称" name="nickname">
-            <Input></Input>
+          <Form.Item
+            label="昵称"
+            name="nickname"
+            rules={[{ max: 20, message: '昵称最多20个字符' }]}
+          >
+            <Input placeholder="可选"></Input>
           </Form.Item>
           <Form.Item wrapperCol={{ offset: 6, span: 20 }}>
             <Space>
